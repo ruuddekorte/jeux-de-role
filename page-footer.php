2223c62cc3557ce62f6dbfc497c58db348677a66
simplon <dekorte.tsrit@gmail.com>
2018-03-15 15:27:37 +0100

    remove obsolete

--- page-footer.php
+++
@@ -1,9 +0,0 @@
-
-
-<!-- ########## FOOTER ########## -->
-	<footer>
-		<h3> Simplon War, un jeu proposé par la team #01 de Simplon Rodez</h3>  
-		<hr />
-		<p><a href="#"> Simplon.co</a> | <a href="#"> Github Simplon Rodez</a></p>
-	</footer>
-	
